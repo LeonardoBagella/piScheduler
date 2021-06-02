610cd2d6c2cc644adf15cde6185f4ef32291654d
Leonardo Bagella <leonardo.bagella@gmail.com>
2021-06-03 01:00:02 +0200

    update

--- calendarTesting.js
+++ calendarTesting.js
@@ -3,39 +3,42 @@ const axios = require('axios');
 const serverURL = "http://localhost:3000";
 
 /*
-testWriteKeepAlive('CAM-1', 'ACTIVE');
-testWriteKeepAlive('CAM-2', 'INACTIVE');
-testWriteKeepAlive('CAM-3', 'ACTIVE');
-*/
-/*
-testReadKeepAlive('CAM-3');
-testReadKeepAlive('CAM-2');
+testWriteKeepAlive('CAM-001', 'ACTIVE');
+
+testWriteKeepAlive('CAM-002', 'INACTIVE');
+testWriteKeepAlive('CAM-003', 'ACTIVE');
 */
 
-//testReadKeepAlive('CAM-2');
+/*
+testReadKeepAlive('CAM-003');
+testReadKeepAlive('CAM-002');
+*/
 
 /*
-testRegisterEvent ('CAM-3', 
+testRegisterEvent ('CAM-001', 
     {
         basicData : {
-        startDate : '2021-05-30T17:00:00',
-        endDate : '2021-05-30T19:00:00',
-        title : "Evento di prova 5",
-        lastScreenshot : ""
+            startDate : '2021-06-02T23:30:00',
+            endDate : '2021-06-03T02:00:00',
+            title : "Evento di prova 9",
+            lastScreenshot : ""
         },
         streamData : {
             streamURL : "http://testImageStream.com/",
-            streamKey : "asaset-4asdfsd-2453454-sdfsd444"
+            streamKey : "asaset-4asdfsd-2453454-sdfsd449"
         }
-}
+    }       
 );
 */
 
-//testReadEventDetails ('1622385394447');
+//testReadEventDetails ('1622664687');
+//testReadEventDetails ('1622664742');
 
 //testReadNextEvents(5);
 
-testReadDailyEvents({ startDay : new Date('2021-05-30T00:00:00'), endDay : new Date('2021-05-31T00:00:00') });
+//testReadNextEventsOnRB('CAM-003', 2);
+
+//testReadEventsInRange({ startDay : '2021-06-02T00:00:00', endDay : '2021-06-04T00:00:00' });
 
 
 /////////////////////////////////////
@@ -106,8 +109,20 @@ function testReadNextEvents(days){
         });
     }
 
-function testReadDailyEvents(jsonDateRange){
-    axios.post(serverURL + '/event/dailyEvents', jsonDateRange)
+function testReadNextEventsOnRB(idRberry, nEvents){
+    axios.get(serverURL + '/rBerry/nextEvents/' + idRberry + "/" + nEvents)
+        .then( res => {
+            console.log(res.data);
+            //console.log(res);
+        })
+        .catch( error => {
+            console.error(error);
+        });
+    }
+
+function testReadEventsInRange(jsonDateRange){
+    //   (max. 50 results)
+    axios.post(serverURL + '/event/rangedEvents', jsonDateRange)
         .then( res => {
             console.log(res.data);
             //console.log(res);
